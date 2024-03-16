4d15e64284bc6530af78be11265273f3996071c7
Evan Chen <hybridcec@gmail.com>
2024-03-16 02:39:20 -0700

    Added greater than and other functions

--- lookup.h
+++ lookup.h
@@ -8,13 +8,16 @@
 
 uint8_t l_get_first_bit(uint8_t x);
 uint8_t l_right_shift_one(uint8_t x);
+uint8_t l_left_shift_one(uint8_t x);
 uint8_t l_set_last_bit(uint8_t x, uint8_t one_bit);
 uint8_t l_concat_bits(uint8_t one_a, uint8_t one_b);
 uint8_t l_xor_bits(uint8_t one_a, uint8_t one_b);
 uint8_t l_and_bits(uint8_t one_a, uint8_t one_b);
 uint8_t l_or_bits(uint8_t one_a, uint8_t one_b);
+uint8_t l_not_bits(uint8_t one_a);
 uint8_t l_xor(uint8_t a, uint8_t b);
 void l_add_bits(uint8_t one_a, uint8_t one_b, uint8_t one_c, uint8_t* sum, uint8_t* carry);
 uint8_t l_add(uint8_t a, uint8_t b);
+uint8_t l_greater_than(uint8_t a, uint8_t b);
 
 #endif
--- test.cpp
+++ test.cpp
@@ -14,6 +14,10 @@ int main (int argc, char** argv) {
     }
 
     for (uint16_t i = 0; i < std::numeric_limits<uint8_t>::max(); i++) {
+        assert (static_cast<uint8_t>(i << 1) == l_left_shift_one(i));
+    }
+
+    for (uint16_t i = 0; i < std::numeric_limits<uint8_t>::max(); i++) {
         assert ((i & ~(1 << 7)) == l_set_last_bit(i, 0));
         assert ((i | (1 << 7)) == l_set_last_bit(i, 1));
     }
@@ -47,6 +51,12 @@ int main (int argc, char** argv) {
     for (uint16_t i = 0; i < std::numeric_limits<uint8_t>::max(); i++) {
         for (uint16_t j = 0; j < std::numeric_limits<uint8_t>::max(); j++) {
             assert (static_cast<uint8_t>(i + j) == l_add(i, j));
+        }
+    }
+
+    for (uint16_t i = 0; i < std::numeric_limits<uint8_t>::max(); i++) {
+        for (uint16_t j = 0; j < std::numeric_limits<uint8_t>::max(); j++) {
+            assert ((i > j ? 1 : 0) == l_greater_than(i, j));
         }
     }
 }
