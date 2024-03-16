2b303c15d5f710eb352e9653592f19b865050d28
Evan Chen <hybridcec@gmail.com>
2024-03-16 02:14:48 -0700

    Initial commit

---
+++ lookup.h
@@ -0,0 +1,19 @@
+#ifndef LOOKUP_H
+#define LOOKUP_H
+
+#include <cstdint>
+
+#define REPEAT(n) for (int repeat_index = 0; repeat_index < n; repeat_index++)
+
+uint8_t l_get_first_bit(uint8_t x);
+uint8_t l_right_shift_one(uint8_t x);
+uint8_t l_set_last_bit(uint8_t x, uint8_t one_bit);
+uint8_t l_concat_bits(uint8_t one_a, uint8_t one_b);
+uint8_t l_xor_bits(uint8_t one_a, uint8_t one_b);
+uint8_t l_and_bits(uint8_t one_a, uint8_t one_b);
+uint8_t l_or_bits(uint8_t one_a, uint8_t one_b);
+uint8_t l_xor(uint8_t a, uint8_t b);
+void l_add_bits(uint8_t one_a, uint8_t one_b, uint8_t one_c, uint8_t* sum, uint8_t* carry);
+uint8_t l_add(uint8_t a, uint8_t b);
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,52 @@
+#include <limits>
+#include <cassert>
+#include <iostream>
+
+#include "lookup.h"
+
+int main (int argc, char** argv) {
+    for (uint16_t i = 0; i < std::numeric_limits<uint8_t>::max(); i++) {
+        assert ((i & 1) == l_get_first_bit(i));
+    }
+
+    for (uint16_t i = 0; i < std::numeric_limits<uint8_t>::max(); i++) {
+        assert ((i >> 1) == l_right_shift_one(i));
+    }
+
+    for (uint16_t i = 0; i < std::numeric_limits<uint8_t>::max(); i++) {
+        assert ((i & ~(1 << 7)) == l_set_last_bit(i, 0));
+        assert ((i | (1 << 7)) == l_set_last_bit(i, 1));
+    }
+
+    assert (0 == l_concat_bits(0, 0));
+    assert (1 == l_concat_bits(0, 1));
+    assert (2 == l_concat_bits(1, 0));
+    assert (3 == l_concat_bits(1, 1));
+
+    assert (0 == l_xor_bits(0, 0));
+    assert (1 == l_xor_bits(0, 1));
+    assert (1 == l_xor_bits(1, 0));
+    assert (0 == l_xor_bits(1, 1));
+
+    assert (0 == l_and_bits(0, 0));
+    assert (0 == l_and_bits(0, 1));
+    assert (0 == l_and_bits(1, 0));
+    assert (1 == l_and_bits(1, 1));
+
+    assert (0 == l_or_bits(0, 0));
+    assert (1 == l_or_bits(0, 1));
+    assert (1 == l_or_bits(1, 0));
+    assert (1 == l_or_bits(1, 1));
+
+    for (uint16_t i = 0; i < std::numeric_limits<uint8_t>::max(); i++) {
+        for (uint16_t j = 0; j < std::numeric_limits<uint8_t>::max(); j++) {
+            assert ((i ^ j) == l_xor(i, j));
+        }
+    }
+
+    for (uint16_t i = 0; i < std::numeric_limits<uint8_t>::max(); i++) {
+        for (uint16_t j = 0; j < std::numeric_limits<uint8_t>::max(); j++) {
+            assert (static_cast<uint8_t>(i + j) == l_add(i, j));
+        }
+    }
+}
